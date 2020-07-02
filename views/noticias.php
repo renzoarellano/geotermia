4fc18cddf6118fe7bfecae5b4a05caa1e7cd5b53
Renzo M. Arellano <wmunozarellano@gmail.com>
2020-07-02 16:11:16 -0500

    [UPDATE] Link Noticias

--- views/noticias.php
+++ views/noticias.php
@@ -12,3 +12,211 @@
 
 	</div>
 </section>
+
+<section class="noticiasSection col-12 np">
+	<div class="container">
+		<div class="paddingSection col-12 np">
+			<div class="row">
+				<div class="col-12 col-md-12 col-lg-6 ">
+					<div class="hrNoticiaDestacada">
+					</div>
+					<div class="fechaNoticiaDestacada2">
+						<img class="calendarIcon" src="app/img/noticias/calendar.png" alt="">
+						Enero 28, 2020
+					</div>
+					<div class="tituloNoticiaDestacada">
+						LOS 10 PRINCIPALES PAÍSESE GEOTÉRMICOS A DICIEMBRE DE 2019: SEGÚN LA CAPACIDAD DE GENERACIÓN
+						INSTALADA (MWE)
+					</div>
+					<div class="descripcionNoticiaDestacada">
+						La capacidad geotérmica instalada a nivel global alcanzó 15,406 MW a fines del año 2019. Durante
+						el año se agregaron 759 MW de capacidad, siendo uno de los mayores crecimientos anuales en los
+						últimos 20 años.
+					</div>
+				</div>
+				<div class="col-12 col-md-12 col-lg-6 np">
+					<img src="app/img/noticias/" alt="">
+				</div>
+			</div>
+
+			<div class="col-12 paddingMidleSection np">
+				<div class="row">
+					<div class="col-12 col-md-6 col-lg-4 noticiaContainer">
+						<a href="#">
+							<div class="imagenNoticia2">
+								<img class="imgResponsive" src="app/img/noticias/not1.jpg" alt="">
+							</div>
+							<div class="fechaNoticiaDestacada2">
+								<img class="calendarIcon" src="app/img/noticias/calendar.png" alt="">
+								Enero 28, 2020
+							</div>
+							<div class="tituloNoticia2">
+								CADE 2019: Debe ampliarse tope de
+								5% de participación de las RER en la
+								matriz energética, dice Osinergmin
+							</div>
+						</a>
+					</div>
+					<div class="col-12 col-md-6 col-lg-4 noticiaContainer">
+						<a href="#">
+							<div class="imagenNoticia2">
+								<img class="imgResponsive" src="app/img/noticias/not2.jpg" alt="">
+							</div>
+							<div class="fechaNoticiaDestacada2">
+								<img class="calendarIcon" src="app/img/noticias/calendar.png" alt="">
+								Enero 21, 2020
+							</div>
+							<div class="tituloNoticia2">
+								Conoce el potencial de Moquegua
+								para generar energía limpia gracias
+								a recursos geotérmicos
+							</div>
+						</a>
+					</div>
+
+					<div class="col-12 col-md-6 col-lg-4 noticiaContainer ">
+						<a href="#">
+							<div class="imagenNoticia2">
+								<img class="imgResponsive" src="app/img/noticias/not3.jpg" alt="">
+							</div>
+							<div class="fechaNoticiaDestacada2">
+								<img class="calendarIcon" src="app/img/noticias/calendar.png" alt="">
+								Agosto 01, 2017
+							</div>
+							<div class="tituloNoticia2">
+								Gobierno Regional de Arequipa, en
+								Perú, declaró de interés público la
+								inversión en energía geotérmica
+							</div>
+						</a>
+					</div>
+
+				</div>
+				<div class="row">
+					<div class="col-12 alignButtons np">
+						<button class="noticiasButton" type="button">
+							<img src="app/img/noticias/left-arrow-noticias.jpg" alt="Arrow Left Noticias">
+						</button>
+						<button class="noticiasButton" type="button">
+							<img src="app/img/noticias/right-arrow-noticias.jpg" alt="Arrow Left Noticias">
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</section>
+<section class="noticiasInternaciolaes colorSection paddingSection col-12 np">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-6 col-lg-4 noticiaContainer ">
+				<a href="#">
+					<div class="imagenNoticia2">
+						<img class="imgResponsive" src="app/img/noticias/not4.jpg" alt="">
+					</div>
+					<div class="fechaNoticiaDestacada2">
+						<img class="calendarIcon" src="app/img/noticias/calendar.png" alt="">
+						Mayo 16, 2020
+					</div>
+					<div class="tituloNoticia2">
+						Becas del Consejo de Recursos
+						Geotérmicos 2020: fecha límite
+						extendida hasta el 29 de mayo 2020
+					</div>
+				</a>
+			</div>
+			<div class="col-12 col-md-6 col-lg-4 noticiaContainer ">
+				<a href="#">
+					<div class="imagenNoticia2">
+						<img class="imgResponsive" src="app/img/noticias/not5.jpg" alt="">
+					</div>
+					<div class="fechaNoticiaDestacada2">
+						<img class="calendarIcon" src="app/img/noticias/calendar.png" alt="">
+						Mayo 11, 2020
+					</div>
+					<div class="tituloNoticia2">
+						España: Calculator, la aplicación
+						que sirve para estimar el potencial
+						geotérmico de acuíferos profundos
+					</div>
+				</a>
+			</div>
+			<div class="col-12 col-md-6 col-lg-4 noticiaContainer ">
+				<a href="#">
+					<div class="imagenNoticia2">
+						<img class="imgResponsive" src="app/img/noticias/not6.jpg" alt="">
+					</div>
+					<div class="fechaNoticiaDestacada2">
+						<img class="calendarIcon" src="app/img/noticias/calendar.png" alt="">
+						Mayo 6, 2020
+					</div>
+					<div class="tituloNoticia2">
+						El DOE de EE. UU. abre la fase de
+						presentación para el Premio de
+						Fabricación Geotérmica Estadounidense
+					</div>
+				</a>
+			</div>
+			<div class="col-12 col-md-6 col-lg-4 noticiaContainer ">
+				<a href="#">
+					<div class="imagenNoticia2">
+						<img class="imgResponsive" src="app/img/noticias/not7.jpg" alt="">
+					</div>
+					<div class="fechaNoticiaDestacada2">
+						<img class="calendarIcon" src="app/img/noticias/calendar.png" alt="">
+						Mayo 6, 2020
+					</div>
+					<div class="tituloNoticia2">
+						Dinamarca cuenta con un “enorme
+						potencial geotérmico”, pero ¿cuánto
+						se puede usar?
+					</div>
+				</a>
+			</div>
+			<div class="col-12 col-md-6 col-lg-4 noticiaContainer ">
+				<a href="#">
+					<div class="imagenNoticia2">
+						<img class="imgResponsive" src="app/img/noticias/not8.jpg" alt="">
+					</div>
+					<div class="fechaNoticiaDestacada2">
+						<img class="calendarIcon" src="app/img/noticias/calendar.png" alt="">
+						Mayo 5, 2020
+					</div>
+					<div class="tituloNoticia2">
+						Colombia será líder en proyecto de
+						la UNESCO sobre recursos geotérmicos
+						para la transición energética
+					</div>
+				</a>
+			</div>
+			<div class="col-12 col-md-6 col-lg-4 noticiaContainer ">
+				<a href="#">
+					<div class="imagenNoticia2">
+						<img class="imgResponsive" src="app/img/noticias/not9.jpg" alt="">
+					</div>
+					<div class="fechaNoticiaDestacada2">
+						<img class="calendarIcon" src="app/img/noticias/calendar.png" alt="">
+						Mayo 5, 2020
+					</div>
+					<div class="tituloNoticia2">
+						El importante papel de la mujer
+						en la industria geotérmica de
+						El Salvador
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 alignButtons np">
+				<button class="noticiasButton" type="button">
+					<img src="app/img/noticias/left-arrow-noticias.jpg" alt="Arrow Left Noticias">
+				</button>
+				<button class="noticiasButton" type="button">
+					<img src="app/img/noticias/right-arrow-noticias.jpg" alt="Arrow Left Noticias">
+				</button>
+			</div>
+		</div>
+	</div>
+
+</section>
